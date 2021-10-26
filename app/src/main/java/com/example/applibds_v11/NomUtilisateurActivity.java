5bb1cdb39296d9e6303a107777c16b7cee4c1ee4
Clémence Garnier <clemence.garnier@edu.esiee.fr>
2021-10-26 13:14:16 +0200

    Ajout layout nom utilisateur

---
+++ app/src/main/java/com/example/applibds_v11/NomUtilisateurActivity.java
@@ -0,0 +1,14 @@
+package com.example.applibds_v11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NomUtilisateurActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nom_utilisateur);
+    }
+}
